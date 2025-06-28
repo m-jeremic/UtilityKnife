a023259465e2c3be4a9d409f4123482556010992
m-jeremic <jerremic@gmail.com>
2025-06-28 17:24:41 +0200

    First commit to main branch

---
+++ UtilityKnife/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UtilityKnife
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // skeleton program
+        }
+    }
+}
