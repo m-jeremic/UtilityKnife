9f0f1c37cbad5c2064c2128ffa4b971615982701
m-jeremic <jerremic@gmail.com>
2025-06-28 17:52:42 +0200

    Add method Subtraction

--- UtilityKnife/Calculator/Calculator.cs
+++ UtilityKnife/Calculator/Calculator.cs
@@ -4,9 +4,14 @@ namespace UtilityKnife.Calculator
 {
     public class Calculator
     {
-        public static  int Add(int x, int y)
+        public static  double Add(double x, double y)
         {
             return x + y;
+        }
+
+        public static double Subtraction(double x, double y)
+        {
+            return x - y;
         }
     }
 }
